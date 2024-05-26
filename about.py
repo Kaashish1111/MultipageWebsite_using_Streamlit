cfa358c78af07132eb6ed0a6aacbd4546f5eb663
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2024-05-26 23:06:54 +0530

    Add files via upload

---
+++ about.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def app():
+    st.subheader('Pondering is a website created for users to')
+    st.subheader('share their valuable thoughts with the world.')
+    st.markdown('Created by: [Kashish Goyal](https://github.com/Kaashish1111)')
+    st.markdown('Contact via mail: [yupkashish@gmail.com]')
